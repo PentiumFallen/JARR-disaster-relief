5332df4b37678db5dc258e127b1a87533f6d9a87
jaimercl <jaime.correa@upr.edu>
2020-03-31 03:26:29 -0400

    update authentication

--- backend/dao/authentication.py
+++ backend/dao/authentication.py
@@ -9,51 +9,40 @@ class AuthenticationDAO:
                                                             pg_config['passwd'])
         self.conn = psycopg2._connect(connection_url)
 
-    def getAllAuthentication(self):
+    def getAllAuthenticationByEmail(self):
         cursor = self.conn.cursor()
-        query = "select * from account;"
+        query = "select * from accounts where email = %s;"
         cursor.execute(query)
         result = []
         for row in cursor:
             result.append(row)
         return result
-
-    def getAuthenticationById(self, id):
+    
+    def getAccountData(self, email, password):
         cursor = self.conn.cursor()
-        query = "select account_id, email, password, is_admin from requests where account_id = %s;"
-        cursor.execute(query, (id,))
+        query = "select * from accounts where email = %s and password = %s;"
+        cursor.execute(query, (email, password))
+        result = cursor.fetchone()
+        return result
+    
+    def getAccountType(self, account_id):
+        cursor = self.conn.cursor()
+        query = "select is_admin from accounts where account_id = %s;"
+        cursor.execute(query, account_id)
         result = cursor.fetchone()
         return result
 
-    def getAuthenticationByPersonId(self, person_id):
+    def insertAccount(self, json):
+        email = json['email']
+        password = json['password']
+        is_admin = json['is_admin']
         cursor = self.conn.cursor()
-        query = "select account_id, email, password, is_admin from person where account_id = %s;"
-        cursor.execute(query, (person_id,))
-        result = cursor.fetchone()
-        return result
-
-    def getAuthenticationByRequestId(self, request_id):
-        cursor = self.conn.cursor()
-        query = "select account_id, email, password, is_admin from person where account_id = %s;"
-        cursor.execute(query, (request_id,))
-        result = cursor.fetchone()
-        return result
-
-    def getAuthenticationBySupplyId(self, supply_id):
-        cursor = self.conn.cursor()
-        query = "select account_id, email, password, is_admin from person where account_id = %s;"
-        cursor.execute(query, (supply_id,))
-        result = cursor.fetchone()
-        return result
-
-    def insertAccount(self, email, password, is_admin):
-        cursor = self.conn.cursor()
-        query = "insert into account(email, password, is_admin) values (%s, %s, %s) returning account_id;"
+        query = "insert into accounts(email, password, is_admin) values (%s, %s, %s) returning account_id;"
         cursor.execute(query, (email, password, is_admin,))
-        account_id = cursor.fetchone()[0]
         self.conn.commit()
-        return account_id
-
+        result = cursor.fetchone()
+        return result
+    
     def deleteAccount(self, account_id):
         cursor = self.conn.cursor()
         query = "delete from account where account_id = %s;"
@@ -61,11 +50,23 @@ class AuthenticationDAO:
         self.conn.commit()
         return account_id
 
-    def updateAccount(self, email, password, is_admin, account_id):
+    def accountChangePassword(self, email,password):
         cursor = self.conn.cursor()
-        query = "update account set email = %s, password = %s, is_admin = %s;"
-        cursor.execute(query, (email, password, is_admin, account_id,))
+        query = "update accounts set password =%s  where email = %s;"
+        cursor.execute(query, (password,email))
         self.conn.commit()
-        return account_id
+        return'Password as been change'
+    
+    def accountLogin(self, email,password):
+        cursor = self.conn.cursor()
+        query = "select * from accounts where email = %s and password = %s;"
+        cursor.execute(query, (email,password,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
 
-        
+
+    
+    
+    
