4353b9462a12ac9034c3c0a88449659bf898f992
Anibal Pagan <anibal.pagan@upr.edu>
2020-05-04 11:19:03 -0400

    Merge branch 'master' of /Users/apaga/PycharmProjects/JARR-disaster-relief with conflicts.

--- backend/dao/supply.py
+++ backend/dao/supply.py
@@ -12,108 +12,245 @@ class SupplyDAO:
 
     def getAllSupplies(self):
         cursor = self.conn.cursor()
-        query = "select * from Supply;"
+        query = "select supply_id, category, subcategory, person_id, name, quantity, brand, sdescription, available, "\
+                "sunit_price, date_offered, address_id from Supplies natural inner join Resources natural inner join " \
+                "(select category_id,category, subcategory from Categories as C left join Subcategories as S on " \
+                "C.subcategory_id = S.subcategory_id);"
         cursor.execute(query)
-        result = cursor.fetchall()
-        # for row in cursor:
-        #     result.append(row)
+        result = []
+        for row in cursor:
+            result.append(row)
         return result
 
-    def getSupplyById(self, sid):
+    def getTotalSupplies(self):
         cursor = self.conn.cursor()
-        query = "select * from Supply where supply_id = %s;"
-        cursor.execute(query, (sid,))
+        query = "select count(*) from Supplies;"
+        cursor.execute(query)
+        result = int(cursor.fetchone())
+        return result
+
+    def getTotalSuppliesPerCategory(self):
+        cursor = self.conn.cursor()
+        query = "select category, count(*) from Supplies natural inner join Resources natural inner join Categories " \
+                "group by category order by category;"
+        cursor.execute(query)
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getTotalAvailableSupplies(self):
+        cursor = self.conn.cursor()
+        query = "select count(*) from Supplies where available > 0;"
+        cursor.execute(query)
+        result = int(cursor.fetchone())
+        return result
+
+    def getTotalAvailableSuppliesPerCategory(self):
+        cursor = self.conn.cursor()
+        query = "select category, count(*) from Supplies natural inner join Resources natural inner join Categories " \
+                " where available > 0 group by category order by category;"
+        cursor.execute(query)
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getAllAvailableSupplies(self):
+        cursor = self.conn.cursor()
+        query = "select supply_id, category, subcategory, person_id, name, quantity, brand, sdescription, available, "\
+                "sunit_price, date_offered, address_id from Supplies natural inner join Resources natural inner join (select category_id, " \
+                "category, subcategory from Categories as C left join Subcategories as S on C.subcategory_id = " \
+                "S.subcategory_id) where available > 0;"
+        cursor.execute(query)
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getSupplyById(self, supply_id):
+        cursor = self.conn.cursor()
+        query = "select supply_id, category, subcategory, person_id, name, quantity, brand, sdescription, available, "\
+                "sunit_price, date_offered, address_id from Supplies natural inner join Resources natural inner join (select category_id, " \
+                "category, subcategory from Categories as C left join Subcategories as S on C.subcategory_id = " \
+                "S.subcategory_id) where supply_id = %s;"
+        cursor.execute(query, (supply_id,))
         result = cursor.fetchone()
-        return result
-
-    def getSuppliesByCategory(self, category):
-        cursor = self.conn.cursor()
-        query = "select * from Supply where scategory = %s;"
-        cursor.execute(query, (category,))
-        result = []
-        for row in cursor:
-            result.append(row)
-        return result
-
-    def getSuppliesByAddress(self, address):
-        cursor = self.conn.cursor()
-        query = "select * from Supply where saddress = %s;"
-        cursor.execute(query, (address,))
-        result = []
-        for row in cursor:
-            result.append(row)
-        return result
-
-    def getSuppliesByCategoryAndAddress(self, category, address):
-        cursor = self.conn.cursor()
-        query = "select * from Supply where scategory = %s and saddress = %s;"
-        cursor.execute(query, (category, address))
-        result = []
-        for row in cursor:
-            result.append(row)
-        return result
-
-    def getSuppliesByCategoryAndMaxPrice(self, category, max_price):
-        cursor = self.conn.cursor()
-        query = "select * from Supply where scategory = %s and sprice <= %s;"
-        cursor.execute(query, (category, max_price))
-        result = []
-        for row in cursor:
-            result.append(row)
-        return result
-
-    def getSuppliesByCategoryAddressAndMaxPrice(self, category, address, max_price):
-        cursor = self.conn.cursor()
-        query = "select * from Supply where scategory = %s and saddress = %s and sprice <= %s;"
-        cursor.execute(query, (category, address, max_price))
-        result = []
-        for row in cursor:
-            result.append(row)
         return result
 
     def getSuppliesByPersonId(self, person_id):
         cursor = self.conn.cursor()
-        query = "select * from Supply natural inner join supplies where person_id = %s;"
+        query = "select supply_id, category, subcategory, person_id, name, quantity, brand, sdescription, available, "\
+                "sunit_price, date_offered, address_id from Supplies natural inner join Resources natural inner join (select category_id, " \
+                "category, subcategory from Categories as C left join Subcategories as S on C.subcategory_id = " \
+                "S.subcategory_id) where person_id = %s;"
         cursor.execute(query, (person_id,))
-        result = []
-        for row in cursor:
-            result.append(row)
+        result = cursor.fetchall()
         return result
 
-    def insert(self, scategory, sdescription, saddress, sprice, date_offered, squantity, person_id):
+    def getSuppliesByBrandAndCategoryAndSubcategoryAndMaxPrice(self, brand, category, subcategory, max_price):
+        cursor = self.conn.cursor()
+        query = "select supply_id, category, subcategory, person_id, name, quantity, brand, sdescription, available, " \
+                "sunit_price, date_offered, address_id from Supplies natural inner join Resources natural inner join (select category_id, " \
+                "category, subcategory from Categories as C left join Subcategories as S on C.subcategory_id = " \
+                "S.subcategory_id) where brand = %s and category = %s and subcategory = %s sunit_price < %s;"
+        cursor.execute(query, (brand, category, subcategory, max_price))
+        result = cursor.fetchall()
+        return result
+
+    def getSuppliesByBrandAndCategoryAndSubcategory(self, brand, category, subcategory):
+        cursor = self.conn.cursor()
+        query = "select supply_id, category, subcategory, person_id, name, quantity, brand, sdescription, available, " \
+                "sunit_price, date_offered, address_id from Supplies natural inner join Resources natural inner join (select category_id, " \
+                "category, subcategory from Categories as C left join Subcategories as S on C.subcategory_id = " \
+                "S.subcategory_id) where brand = %s and category = %s and subcategory = %s;"
+        cursor.execute(query, (brand, category, subcategory))
+        result = cursor.fetchall()
+        return result
+
+    def getSuppliesByBrandAndCategoryAndMaxPrice(self, brand, category, max_price):
+        cursor = self.conn.cursor()
+        query = "select supply_id, category, subcategory, person_id, name, quantity, brand, sdescription, available, "\
+                "sunit_price, date_offered, address_id from Supplies natural inner join Resources natural inner join (select category_id, " \
+                "category, subcategory from Categories as C left join Subcategories as S on C.subcategory_id = " \
+                "S.subcategory_id) where brand = %s and category = %s and sunit_price < %s;"
+        cursor.execute(query, (brand, category, max_price))
+        result = cursor.fetchall()
+        return result
+
+    def getSuppliesByBrandAndCategory(self, brand, category):
+        cursor = self.conn.cursor()
+        query = "select supply_id, category, subcategory, person_id, name, quantity, brand, sdescription, available, "\
+                "sunit_price, date_offered, address_id from Supplies natural inner join Resources natural inner join (select category_id, " \
+                "category, subcategory from Categories as C left join Subcategories as S on C.subcategory_id = " \
+                "S.subcategory_id) where brand = %s and category = %s;"
+        cursor.execute(query, (brand, category))
+        result = cursor.fetchall()
+        return result
+
+    def getSuppliesByBrand(self, brand):
+        cursor = self.conn.cursor()
+        query = "select supply_id, category, subcategory, person_id, name, quantity, brand, sdescription, available, "\
+                "sunit_price, date_offered, address_id from Supplies natural inner join Resources natural inner join (select category_id, " \
+                "category, subcategory from Categories as C left join Subcategories as S on C.subcategory_id = " \
+                "S.subcategory_id) where brand = %s;"
+        cursor.execute(query, (brand,))
+        result = cursor.fetchall()
+        return result
+
+    def getSuppliesByCategory(self, category):
+        cursor = self.conn.cursor()
+        query = "select supply_id, category, subcategory, person_id, name, quantity, brand, sdescription, available, "\
+                "sunit_price, date_offered, address_id from Supplies natural inner join Resources natural inner join (select category_id, " \
+                "category, subcategory from Categories as C left join Subcategories as S on C.subcategory_id = " \
+                "S.subcategory_id) where category = %s;"
+        cursor.execute(query, (category,))
+        result = cursor.fetchall()
+        return result
+
+    def getSuppliesByMaxPrice(self, max_price):
+        cursor = self.conn.cursor()
+        query = "select supply_id, category, subcategory, person_id, name, quantity, brand, sdescription, available, "\
+                "sunit_price, date_offered, address_id from Supplies natural inner join Resources natural inner join (select category_id, " \
+                "category, subcategory from Categories as C left join Subcategories as S on C.subcategory_id = " \
+                "S.subcategory_id) where sunit_price < %s;"
+        cursor.execute(query, (max_price,))
+        result = cursor.fetchall()
+        return result
+
+    def getAvailableSuppliesByBrandAndCategoryAndSubcategoryAndMaxPrice(self, brand, category, subcategory, max_price):
+        cursor = self.conn.cursor()
+        query = "select supply_id, category, subcategory, person_id, name, quantity, brand, sdescription, available, " \
+                "sunit_price, date_offered, address_id from Supplies natural inner join Resources natural inner join (select category_id, " \
+                "category, subcategory from Categories as C left join Subcategories as S on C.subcategory_id = " \
+                "S.subcategory_id) where brand = %s and category = %s and subcategory = %s sunit_price < %s and available > 0;"
+        cursor.execute(query, (brand, category, subcategory, max_price))
+        result = cursor.fetchall()
+        return result
+
+    def getAvailableSuppliesByBrandAndCategoryAndSubcategory(self, brand, category, subcategory):
+        cursor = self.conn.cursor()
+        query = "select supply_id, category, subcategory, person_id, name, quantity, brand, sdescription, available, " \
+                "sunit_price, date_offered, address_id from Supplies natural inner join Resources natural inner join (select category_id, " \
+                "category, subcategory from Categories as C left join Subcategories as S on C.subcategory_id = " \
+                "S.subcategory_id) where brand = %s and category = %s and subcategory = %s and available > 0;"
+        cursor.execute(query, (brand, category, subcategory))
+        result = cursor.fetchall()
+        return result
+
+    def getAvailableSuppliesByBrandAndCategoryAndMaxPrice(self, brand, category, max_price):
+        cursor = self.conn.cursor()
+        query = "select supply_id, category, subcategory, person_id, name, quantity, brand, sdescription, available, " \
+                "sunit_price, date_offered, address_id from Supplies natural inner join Resources natural inner join (select category_id, " \
+                "category, subcategory from Categories as C left join Subcategories as S on C.subcategory_id = " \
+                "S.subcategory_id) where brand = %s and category = %s and sunit_price < %s and available > 0;"
+        cursor.execute(query, (brand, category, max_price))
+        result = cursor.fetchall()
+        return result
+
+    def getAvailableSuppliesByBrandAndCategory(self, brand, category):
+        cursor = self.conn.cursor()
+        query = "select supply_id, category, subcategory, person_id, name, quantity, brand, sdescription, available, " \
+                "sunit_price, date_offered, address_id from Supplies natural inner join Resources natural inner join (select category_id, " \
+                "category, subcategory from Categories as C left join Subcategories as S on C.subcategory_id = " \
+                "S.subcategory_id) where brand = %s and category = %s and available > 0;"
+        cursor.execute(query, (brand, category))
+        result = cursor.fetchall()
+        return result
+
+    def getAvailableSuppliesByBrand(self, brand):
+        cursor = self.conn.cursor()
+        query = "select supply_id, category, subcategory, person_id, name, quantity, brand, sdescription, available, " \
+                "sunit_price, date_offered, address_id from Supplies natural inner join Resources natural inner join (select category_id, " \
+                "category, subcategory from Categories as C left join Subcategories as S on C.subcategory_id = " \
+                "S.subcategory_id) where brand = %s and available > 0;"
+        cursor.execute(query, (brand,))
+        result = cursor.fetchall()
+        return result
+
+    def getAvailableSuppliesByCategory(self, category):
+        cursor = self.conn.cursor()
+        query = "select supply_id, category, subcategory, person_id, name, quantity, brand, sdescription, available, " \
+                "sunit_price, date_offered, address_id from Supplies natural inner join Resources natural inner join (select category_id, " \
+                "category, subcategory from Categories as C left join Subcategories as S on C.subcategory_id = " \
+                "S.subcategory_id) where category = %s and available > 0;"
+        cursor.execute(query, (category,))
+        result = cursor.fetchall()
+        return result
+
+    def getAvailableSuppliesByMaxPrice(self, max_price):
+        cursor = self.conn.cursor()
+        query = "select supply_id, category, subcategory, person_id, name, quantity, brand, sdescription, available, " \
+                "sunit_price, date_offered, address_id from Supplies natural inner join Resources natural inner join (select category_id, " \
+                "category, subcategory from Categories as C left join Subcategories as S on C.subcategory_id = " \
+                "S.subcategory_id) where sunit_price < %s and available > 0;"
+        cursor.execute(query, (max_price,))
+        result = cursor.fetchall()
+        return result
+
+    def insert(self, resource_id, person_id, description, available, unit_price, address_id):
         cursor = self.conn.cursor()
 
-        query = "insert into Supply(scategory, sdescription, saddress, sprice) values (%s, %s, %s, %s) returning supply_id;"
-        cursor.execute(query, (scategory, sdescription, saddress, sprice,))
+        query = "insert into Supplies(resource_id, person_id, description, available, sunit_price, " \
+                "address_id) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) returning supply_id;"
+        cursor.execute(query, (resource_id, person_id, description, available, unit_price, address_id))
 
         supply_id = cursor.fetchone()[0]
-
-        query2 = "insert into Supply(date_offered, squantity, person_id, supply_id) values (%s, %s, %s, %s)"
-        cursor.execute(query2, (date_offered, squantity, person_id, supply_id,))
 
         self.conn.commit()
         return supply_id
 
-    # Shouldn't use!
     def delete(self, supply_id):
         cursor = self.conn.cursor()
-        query = "delete from Supply where supply_id = %s;"
+        query = "update Supplies set available = 0 where supply_id = %s;"
         cursor.execute(query, (supply_id,))
         self.conn.commit()
         return supply_id
 
-    def update(self, supply_id, scategory, sdescription, saddress, sprice):
+    def update(self, supply_id, description, available, unit_price, address_id):
         cursor = self.conn.cursor()
-        query = "update Supply set scategory = %s, sdescription = %s, saddress = %s, sprice = %s where supply_id = %s;"
-        cursor.execute(query, (scategory, sdescription, saddress, sprice, supply_id,))
+        query = "update Supplies set description = %s, available = %s, sunit_price = %s, address_id = %s where " \
+                "supply_id = %s;"
+        cursor.execute(query, (supply_id, description, available, unit_price, address_id))
         self.conn.commit()
         return supply_id
-
-    def getCountBySupplyCategory(self):
-        cursor = self.conn.cursor()
-        query = "select scategory, sum(squantity) from Supply natural inner join Supplies group by scategory, scategory order by scategory;"
-        cursor.execute(query)
-        result = []
-        for row in cursor:
-            result.append(row)
-        return result
